e57dcf37de910782267b7963ce4b165378fd9e33
Ian Zimmerman <ianzimmerman@gmail.com>
2017-01-30 18:55:24 -0800

    design changes (badge)

--- gifoff/blueprints/tournament/controllers.py
+++ gifoff/blueprints/tournament/controllers.py
@@ -7,13 +7,13 @@ from flask_security import current_user, login_required, roles_required
 
 from .helpers import IDSlugConverter, add_app_url_map_converter
 from ...cache import cache, clear_keys
-from ...forms import GroupForm, ChallengeEntry, ChallengeForm, PromptForm
+from ...forms import TournamentForm
 from ...mail import send_async_email
-from ...models import db, db_commit, User, Group, Challenge, Entry, Prompt, Rating, FFARating
+from ...models import db, db_commit, Tournament, TournamentEntry, TournamentPlayers, TournamentRound, TournamentVote, Rating
 
 Blueprint.add_app_url_map_converter = add_app_url_map_converter
 
-tournament = Blueprint('tournament', __name__, url_prefix='/tournament', template_folder='templates')
+tournament = Blueprint('tournament', __name__, url_prefix='/tournaments', template_folder='templates')
 tournament.add_app_url_map_converter(IDSlugConverter, 'id_slug')
 
 
